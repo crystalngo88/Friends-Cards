bdf4b0dcae5a833132f00fc649982ff0e03bc749
Crystal Ngo <crystalngo88@gmail.com>
2018-09-30 19:15:15 -0700

    everything works, pictures shuffle

--- src/App.js
+++ src/App.js
@@ -5,34 +5,71 @@ import Title from "./components/Title";
 import ladies from "./ladies.json";
 import "./App.css";
 
+const array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",]
+
 class App extends Component {
   // Setting this.state.ladies to the ladies json array
   state = {
-    ladies
+    ladies,
+    clickedImages: [],
   };
 
+  _handleClick = (id) => {
+    // 1) Check clicked image ID against array of prev clicks (this.state.clickedImages)
+    if (this.state.clickedImages.includes(id)) {
+      alert("You already clicked this picture. You lose.")
+    } else {
+      var newClickedImages = this.state.clickedImages.slice(0);
+      newClickedImages.push(id)
+      if (newClickedImages.length === ladies.length){
+        alert("Yay. You win.")
+      }
+      var newLadies = this._shuffle(this.state.ladies)
+      this.setState({clickedImages: newClickedImages, ladies: newLadies})
+    }
+    // 2) If matches, game over (run defeat function)
+    // 3) If no match found, add current ID to array of prev clicks
+    // 4) If array of prev clicks === total images, game won (run win function)
+    // 5) If game not over, shuffle existing images
+  }
 
-  //selectLady should save selected id into an array, shuffle images. if selected id = something in array, player loses and gains a Loss Point, if not, add a Win point
-// selectLady = id => {
-//   const ladies = this.state.ladies.filter(lady => lady.id !== id);
-//   this.setState({ ladies });
-// }
+  _shuffle = (array) => {
+    let chickenarray = array.slice(0);
 
-  removeLady = id => {
-    // Filter this.state.ladies for ladies with an id not equal to the id being removed
-    const ladies = this.state.ladies.filter(lady => lady.id !== id);
-    // Set this.state.ladies equal to the new ladies array
-    this.setState({ ladies });
-  };
+    var currentIndex = chickenarray.length; // i.e. 14
+    var temporaryValue = undefined;
+    var randomIndex = undefined;
 
+    // While there remain elements to shuffle...
+    while (currentIndex > 0) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex); // 7
+      currentIndex--; // 13
+
+      // And swap it with the current element.
+      temporaryValue = chickenarray[currentIndex]; // 13
+      chickenarray[currentIndex] = chickenarray[randomIndex];
+      chickenarray[randomIndex] = temporaryValue;
+    }
+
+    return chickenarray;
+  }
+
+  _hamsterFunction = (parameter) => {
+    console.log('hamsters are cute')
+  }
   // Map over this.state.ladies and render a LadyCard component for each friend object
   render() {
+    var newRandomArray = this._shuffle(array);
+    console.log('after randomizing, output is;:', newRandomArray);
+    console.log(this.state.clickedImages)
     return (
       <Wrapper>
         <Title>Ladies List</Title>
         {this.state.ladies.map(lady => (
           <LadyCard
-            removeLady={this.removeLady}
+            handleClick={this._handleClick}
             id={lady.id}
             key={lady.id}
             image={lady.image}
--- src/components/LadyCard/LadyCard.js
+++ src/components/LadyCard/LadyCard.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./LadyCard.css";
 
-const LadyCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      
-    </div>
-    <span onClick={() => props.removeLady(props.id)} className="remove">
-      𝘅
+const LadyCard = props => {
+  return (
+    <div className="card" onClick={function () {
+      props.handleClick(props.id);
+    }}>
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+
+      </div>
+      <span onClick={() => props.removeLady(props.id)} className="remove">
+        𝘅
     </span>
-  </div>
-);
+    </div>
+  )
+};
 
 export default LadyCard;
